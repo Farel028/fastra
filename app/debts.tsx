0b1aebc947397ac36e0addd7b8b8ad47f8029d54
Farel028 <farelardi088@gmail.com>
2026-02-15 22:05:36 +0700

    todo: add debts feature

---
+++ app/debts.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Debts = () => {
+  return (
+    <View>
+      <Text>Debts</Text>
+    </View>
+  );
+};
+
+export default Debts;
+
+const styles = StyleSheet.create({});
